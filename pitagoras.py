3a85a699af921af16f612457957e44389c4cedb7
Rodger Cabrerisso <83619204+rodgerhades@users.noreply.github.com>
2024-08-21 14:04:47 -0300

    Add files via upload

---
+++ pitagoras.py
@@ -0,0 +1,11 @@
+#pitagoras
+#valores c² = a²+b²
+
+a = float(input("valor "))
+b = float(input("valor "))
+calculo = a * a  
+calculo2 = b * b
+resultado = calculo + calculo2
+resultadofinal = resultado
+print("o resultado é",resultadofinal)
+
